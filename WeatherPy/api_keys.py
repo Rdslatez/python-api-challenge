999143c1c60591ba22c5cf9d585ecd0d7139e405
Rdslatez <riley.daniel.slater@gmail.com>
2023-03-27 22:07:43 -0400

    ignore

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8f62f45fb2b5a993d6ef31d166f62ab7"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "a5b01212c28842f394e6eafb483309ab"
